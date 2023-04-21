e7cca03df6f0315a52986ce6ecc1f866080719f4
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-04-21 21:25:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -32,7 +32,7 @@ async def start(client, message):
         ]]  
         reply_markup = InlineKeyboardMarkup(buttons)
         fuck=await message.reply("......")
-        fuck await message.edit(script.STARTER_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
+        await fuck.edit(script.STARTER_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(120) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
